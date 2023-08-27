56947f99fb727d1e194d30e5967d1d89726706ea
Max9222 <maxoog@bk.ru>
2023-08-27 18:36:34 +0300

    completed homework

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,36 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+
+
+CREATE TABLE customers
+(
+	customer_id char(5),
+	company_name varchar(100) NOT NULL,
+	contact_name text
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5),
+	employee_id int, --REFERENCES employees(employee_id) NOT NULL
+	order_date date,
+	ship_city varchar(50)
+);
+
+INSERT INTO employees VALUES (1, 'Nancy', 'Davolio', 'Sales Representative', '1948-12-08', 'Education includes a BA in psychology from Colorado State University in 1970.  She also completed The Art of the Cold Call.  Nancy is a member of Toastmasters International.');
+INSERT INTO customers VALUES ('ALFKI', 'Alfreds Futterkiste', 'Maria Anders');
+INSERT INTO orders VALUES (10248, 'VINET', 5, '1996-07-04', 'Reims');
+
+SELECT * FROM employees
+SELECT * FROM customers
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+all_data = {"customers": "customers_data.csv", "employees": "employees_data.csv", "orders": "orders_data.csv"}
+
+enter = input('Привет, при нажатии "Enter" три таблицы данными из north_data заполнятся')
+
+for table, name_file in all_data.items():
+    print(table)
+    print(name_file)
+
+    """Скрипт для заполнения данными таблиц в БД Postgres."""
+    # connect to bd
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='123')
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                # Добавляем
+                with open(f"north_data/{name_file}", 'r') as csv_file:
+                    items = csv.reader(csv_file)
+                    for i in items:
+                        if len(i) == 3:
+                            cur.execute(f"INSERT INTO {table} VALUES (%s, %s, %s)", (i[0], i[1], i[2]))
+                        elif len(i) == 5:
+                            cur.execute(f"INSERT INTO {table} VALUES (%s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4]))
+                        else:
+                            cur.execute(f"INSERT INTO {table} VALUES (%s, %s, %s, %s, %s, %s)", (i[0], i[1], i[2], i[3], i[4], i[5]))
+
+                # Получаем
+                cur.execute(f"SELECT * FROM {table}")
+                rows = cur.fetchall()
+                print("Все готово")
+
+    finally:
+        conn.close()
